fba6c3413abfe87f9a92fe749b24d74a91d57973
agbamoh <37769188+agbamoh@users.noreply.github.com>
2022-06-19 16:11:36 +0300

    tests for user management

---
+++ unitTests/get_users.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const UsersManager = require("../Logic/Users/UsersManager");
+const Databases = require("../Databases/Databases");
+let root = "C:/Users/mohamad/source/repos/ROOT";
+Databases.init(root + "/" + "db/database.db");
+let manager=new UsersManager();
+
+
+test("test get users1",  ()=>{
+    manager.get_users((users) => {
+        let x = Object.keys(users).length;
+        expect(x).toBe(14);
+    }, (e) => {
+        fail("failed to get users from the db");
+    });
+
+});
+
